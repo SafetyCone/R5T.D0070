fcc5b11a0e085af73c99438ea1b2984ff1a7d541
David Coats <david@getsafetycone.com>
2021-05-27 16:39:08 -0700

    Add Standard service aggregation library.

---
+++ source/R5T.D0070.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Lombardy;
+
+using R5T.D0065;
+
+
+namespace R5T.D0070.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static
+            (
+            IServiceAction<IAppSettingsFilePathProvider> _,
+            IServiceAction<IAppSettingsDirectoryPathProvider> AppSettingsDirectoryPathProviderAction,
+            IServiceAction<IAppSettingsFileNameProvider> AppSettingsFileNameProviderAction
+            )
+        AddDefaultJsonAppSettingsFilePathProviderAction(this IServiceCollection services,
+            IServiceAction<IExecutableDirectoryPathProvider> executableDirectoryPathProviderAction,
+            IServiceAction<IFileNameOperator> fileNameOperatorAction,
+            IServiceAction<IStringlyTypedPathOperator> stringlyTypedPathOperatorAction)
+        {
+            var appSettingsDirectoryPathProviderAction = services.AddExecutableDirectoryBasedAppSettingsDirectoryPathProviderAction(
+                executableDirectoryPathProviderAction);
+
+            var appsettingsFileNameProviderAction = services.AddDefaultJsonAppSettingsFileNameProviderAction(
+                fileNameOperatorAction);
+
+            var appSettingsFilePathProviderAction = services.AddAppSettingsFilePathProviderAction(
+                appSettingsDirectoryPathProviderAction,
+                appsettingsFileNameProviderAction,
+                stringlyTypedPathOperatorAction);
+
+            return
+                (
+                appSettingsFilePathProviderAction,
+                appSettingsDirectoryPathProviderAction,
+                appsettingsFileNameProviderAction
+                );
+        }
+    }
+}
