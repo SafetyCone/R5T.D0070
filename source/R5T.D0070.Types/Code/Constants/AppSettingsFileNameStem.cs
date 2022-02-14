47aa0e3e2e0012af493a65d33fddd6bc9c29af7c
David Coats <david@getsafetycone.com>
2022-02-14 10:48:09 -0800

    Follow-through on moving functionality from the System to the R5T.Magyar namespace.

--- source/R5T.D0070.Types/Code/Constants/AppSettingsFileNameStem.cs
+++ source/R5T.D0070.Types/Code/Constants/AppSettingsFileNameStem.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 
+using R5T.Magyar;
+
 
 namespace R5T.D0070
 {
     public static class AppSettingsFileNameStem
     {
-        public const string DefaultTokenSeparator = Strings.Period;
+        public const string DefaultTokenSeparator = Strings.Period_Const;
     }
 }
